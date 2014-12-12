dbb485dc79987936c8408ce14e38815c21adc7e5
baidut <13591120447@163.com>
2014-12-12 21:49:29 +0800

    添加登陆用户验证 未整合

---
+++ application/views/includes/footer.php
@@ -0,0 +1,7 @@
+
+
+<div id='footer'>
+	<strong>&copy; 2011</strong>
+</div>
+</body>
+</html>
---
+++ application/views/includes/header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+
+<html lang='en'>
+<head>
+   <title><?php if(isset($title))echo $title ?></title>
+   <link rel="stylesheet" href="<?php echo base_url();?>css/style.css" type="text/css" media="print, projection, screen">
+</head>
+<body>
+  <h1><?php if(isset($heading))echo $heading ?></h1>
